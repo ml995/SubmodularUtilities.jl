8140e3457c2cd6ce652141bfcecc25ab9f0d558b
Lin Chen <abratdarcy@gmail.com>
2018-12-02 18:38:00 -0500

    Docs

--- docs/make.jl
+++ docs/make.jl
@@ -5,6 +5,12 @@ makedocs(sitename="SubmodularUtilities.jl")
 # makedocs(sitename="SubmodularUtilities.jl", 
 # 	 repo="https://github.com/lchen91/Submodular_Utilities/blob/master{path}#{line}")
 
+# deploydocs(
+# 	repo = "github.com/lchen91/Submodular_Utilities.git",
+# )
+
 deploydocs(
-	repo = "github.com/lchen91/Submodular_Utilities.git",
+    repo   = "github.com/lchen91/Submodular_Utilities.git",
+    branch = "gh-pages",
+    devbranch = "master",
 )
