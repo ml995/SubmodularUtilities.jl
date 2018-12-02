3120e8d5b0cfa02f808755b6be967377d6402ccb
Lin Chen <abratdarcy@gmail.com>
2018-12-02 16:11:41 -0500

    Docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,10 @@
 push!(LOAD_PATH, "../src")
 using Documenter, SubmodularUtilities
 
-makedocs(sitename="SubmodularUtilities.jl", 
-	 repo="https://github.com/lchen91/Submodular_Utilities/blob/master{path}#{line}")
+makedocs()
+# makedocs(sitename="SubmodularUtilities.jl", 
+# 	 repo="https://github.com/lchen91/Submodular_Utilities/blob/master{path}#{line}")
+
+deploydocs(
+	repo = "github.com/lchen91/Submodular_Utilities.git",
+)
